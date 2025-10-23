fdabd0582f2ba1136680546833a9dd80015f4f53
Anaconder <110943268+Anaconder@users.noreply.github.com>
2025-10-23 10:54:00 -0400

    itemroute_mods

--- routes/cartRoutes.mjs
+++ routes/cartRoutes.mjs
@@ -3,9 +3,10 @@ import Cart from "../models/cartSchema.mjs";
 
 const router = express.Router();
 
-// get our cart
+
 router
   .route("/:id")
+  // get our cart
   .get(async (req, res) => {
     try {
         const cart = await Cart.findById(req.params.id).populate('items.item', "name");
@@ -16,7 +17,7 @@ router
       res.send(err.message);
     }
   })
-  //Update cart Delete cart 
+  //Update cart 
   .put(async (req, res) => {
     try {
       const cart = await Cart.findById(req.params.id);
@@ -32,39 +33,39 @@ router
       console.error(err.message);
       res.send(err.message);
     }
-  });
+
+  })
+  // DELETE a cart by ID
+  .delete(async (req, res) => {
+    try {
+      const deletedCart = await Cart.findByIdAndDelete(req.params.id);
+
+      if (!deletedCart) {
+        return res.status(404).send("Cart not found");
+      }
+
+      res.status(200).json({ message: "Cart deleted successfully" });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(err.message);
+    }
+});
+
+
 
 // create a cart
 router.post("/", async (req, res) => {
   try {
 
 
-    await cart.save();
+    await Cart.save();
 
-    res.json(cart);
+    res.json(Cart);
   } catch (err) {
     console.error(err.message);
     res.send(err.message);
   }
 });
-
-
-// DELETE a cart by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const deletedCart = await Cart.findByIdAndDelete(req.params.id);
-
-    if (!deletedCart) {
-      return res.status(404).send("Cart not found");
-    }
-
-    res.status(200).json({ message: "Cart deleted successfully" });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send(err.message);
-  }
-});
-
 
 
 export default router;
--- routes/itemRoutes.mjs
+++ routes/itemRoutes.mjs
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
   .route("/")
+  //get item
   .get(async (req, res) => {
     try {
       const inventory = await Inventory.find({});
@@ -14,7 +15,7 @@ router
       console.error(err.message);
       res.send(err.message);
     }
-  })
+  })//create item
   .post(async (req, res) => {
     try {
         const newInventory = await Inventory.insertOne(req.body);
@@ -24,6 +25,22 @@ router
       console.error(err.message);
       res.send(err.message);
     }
-  });
+  })
+  .delete(async (req, res) => {
+      try {
+        const deletedItem = await Cart.findByIdAndDelete(req.params.id);
+  
+        if (!deletedItem) {
+          return res.status(404).send("Item not found");
+        }
+  
+        res.status(200).json({ message: "Item deleted successfully" });
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send(err.message);
+      }
+  
+     });
+
 
 export default router;
